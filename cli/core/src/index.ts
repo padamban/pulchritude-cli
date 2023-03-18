e119324b42aa2db0ca9cee297a61aa5ba8a9e63e
Peter Adam <peter.adam.b@gmail.com>
2023-03-18 12:43:51 +0100

    chore: remove utils package

--- cli/core/src/index.ts
+++ cli/core/src/index.ts
@@ -5,7 +5,3 @@ export * from './prettier'
 export * from './progress'
 export * from './report'
 export * from './utils'
-
-export const addCore = (a: number, b: number) => {
-  return a + b + 2000
-}
--- cli/utils/src/index.ts
+++
@@ -1,3 +0,0 @@
-export const addUtils = (a: number, b: number) => {
-  return a + b + 1000
-}
