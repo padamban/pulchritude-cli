6243dc4701b7015d2d49403a60a1c911150ffe89
Peter Adam <peter.adam.b@gmail.com>
2023-04-12 22:58:24 +0200

    fix: tsc build fail

--- cli/core/src/command/index.ts
+++ cli/core/src/command/index.ts
@@ -1,4 +1,10 @@
 export type { CliSetupDetails } from './_type_'
-export type * from './resolve-setup/_type_'
+export type {
+  ArgumentSetup,
+  CliSetup,
+  CommandSetup,
+  OptionSetup,
+  ProgramSetup,
+} from './resolve-setup/_type_'
 export { RESOLVE_SETUP } from './resolve-setup/RESOLVE_SETUP'
 export { SETUP_COMMANDER } from './SETUP_COMMANDER'
