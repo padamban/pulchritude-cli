8b474f481b2789655fa53dcf6df7b3ba6d15fe87
Peter Adam <peter.adam.b@gmail.com>
2023-04-17 23:08:25 +0200

    test: restore e2e tests

--- demo/e2e/src/tests/setup-validation.test.ts
+++ demo/e2e/src/tests/setup-validation.test.ts
@@ -1,4 +1,4 @@
-import { getTerminal } from '../utils/terminal'
+import { getTerminal, TerminalEnvironment } from '../utils/terminal'
 
 describe('CLI setup validator', () => {
   test('shows error if the cli has no programs', async () => {
@@ -59,53 +59,53 @@ describe('CLI setup validator', () => {
     await env?.cleanup()
   })
 
-  // describe('parameter validation found issues', () => {
-  //   let env: TerminalEnvironment | undefined
-  //   let code: number
-  //   let stdout: string[]
+  describe('parameter validation found issues', () => {
+    let env: TerminalEnvironment | undefined
+    let code: number
+    let stdout: string[]
 
-  //   beforeAll(async () => {
-  //     env = await getTerminal()
+    beforeAll(async () => {
+      env = await getTerminal()
 
-  //     await env.writeConfig('invalid-params')
+      await env.writeConfig('invalid-params')
 
-  //     const exe = await env.executeCli(`--help`)
+      const exe = await env.executeCli(`--help`)
 
-  //     code = exe.code
-  //     stdout = exe.stdout
+      code = exe.code
+      stdout = exe.stdout
 
-  //     expect(code).toBe(1)
-  //     expect(stdout).includes('CLI SETUP ISSUES')
-  //   })
+      expect(code).toBe(1)
+      expect(stdout).includes('CLI SETUP ISSUES')
+    })
 
-  //   afterAll(async () => {
-  //     await env?.cleanup()
-  //   })
+    afterAll(async () => {
+      await env?.cleanup()
+    })
 
-  //   test('required arguments must be at the front', async () => {
-  //     expect(stdout).includes(
-  //       'ERROR - The required arguments must be at the front.',
-  //     )
-  //     expect(stdout).includes('> It happened at: program.command')
-  //   })
+    test('required arguments must be at the front', async () => {
+      expect(stdout).includes(
+        'ERROR - The required arguments must be at the front.',
+      )
+      expect(stdout).includes('> It happened at: program.command')
+    })
 
-  //   test('variadic argument must be at the end', async () => {
-  //     expect(stdout).includes('ERROR - Only the last argument can be variadic.')
-  //     expect(stdout).includes('> It happened at: program.command')
-  //   })
+    test('variadic argument must be at the end', async () => {
+      expect(stdout).includes('ERROR - Only the last argument can be variadic.')
+      expect(stdout).includes('> It happened at: program.command')
+    })
 
-  //   test(`argument choice shouldn't have type`, async () => {
-  //     expect(stdout).includes(
-  //       `WARN - The argument's type property is not needed with choices array.`,
-  //     )
-  //     expect(stdout).includes('> It happened at: program.command.arg2')
-  //   })
+    test(`argument choice shouldn't have type`, async () => {
+      expect(stdout).includes(
+        `WARN - The argument's type property is not needed with choices array.`,
+      )
+      expect(stdout).includes('> It happened at: program.command.arg2')
+    })
 
-  //   test(`option choice shouldn't have type`, async () => {
-  //     expect(stdout).includes(
-  //       `WARN - The option's type property is not needed with choices array.`,
-  //     )
-  //     expect(stdout).includes('> It happened at: program.command.opt2')
-  //   })
-  // })
+    test(`option choice shouldn't have type`, async () => {
+      expect(stdout).includes(
+        `WARN - The option's type property is not needed with choices array.`,
+      )
+      expect(stdout).includes('> It happened at: program.command.opt2')
+    })
+  })
 })
