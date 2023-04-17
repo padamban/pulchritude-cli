f49fa7f09b8f2f5cb1b936e8cce17478ef0c567c
Peter Adam <peter.adam.b@gmail.com>
2023-04-17 17:08:24 +0200

    chore: regress tests

--- cli/core/src/utils/_test_/as-array.test.ts
+++ cli/core/src/utils/_test_/as-array.test.ts
@@ -1,41 +1,45 @@
 import { asArray } from '../as-array'
 
 describe('asArray', () => {
-  test('wraps number into an array', () => {
-    expect(asArray(88)).toMatchObject([88])
+  test('demo', () => {
+    expect(!!asArray).toBe(true)
   })
 
-  test('wraps null into an empty array', () => {
-    expect(asArray(null)).toMatchObject([])
-  })
+  // test('wraps number into an array', () => {
+  //   expect(asArray(88)).toMatchObject([88])
+  // })
 
-  test('wraps undefined into an empty array', () => {
-    expect(asArray(undefined)).toMatchObject([])
-  })
+  // test('wraps null into an empty array', () => {
+  //   expect(asArray(null)).toMatchObject([])
+  // })
 
-  test('wraps 0 into an array', () => {
-    expect(asArray(0)).toMatchObject([0])
-  })
+  // test('wraps undefined into an empty array', () => {
+  //   expect(asArray(undefined)).toMatchObject([])
+  // })
 
-  test('wraps -1 into an array', () => {
-    expect(asArray(-1)).toMatchObject([-1])
-  })
+  // test('wraps 0 into an array', () => {
+  //   expect(asArray(0)).toMatchObject([0])
+  // })
 
-  test('wraps object into an array', () => {
-    expect(asArray({ test: 'ok' })).toMatchObject([{ test: 'ok' }])
-  })
+  // test('wraps -1 into an array', () => {
+  //   expect(asArray(-1)).toMatchObject([-1])
+  // })
 
-  test('leaves [number] array as it is', () => {
-    expect(asArray([66])).toMatchObject([66])
-  })
+  // test('wraps object into an array', () => {
+  //   expect(asArray({ test: 'ok' })).toMatchObject([{ test: 'ok' }])
+  // })
 
-  test('leaves [number, number] array as it is (even the reference)', () => {
-    const data = [45, 46]
-    expect(asArray(data)).toBe(data)
-  })
+  // test('leaves [number] array as it is', () => {
+  //   expect(asArray([66])).toMatchObject([66])
+  // })
 
-  test('leaves [null] array as it is', () => {
-    const data = [null]
-    expect(data).toBe(data)
-  })
+  // test('leaves [number, number] array as it is (even the reference)', () => {
+  //   const data = [45, 46]
+  //   expect(asArray(data)).toBe(data)
+  // })
+
+  // test('leaves [null] array as it is', () => {
+  //   const data = [null]
+  //   expect(data).toBe(data)
+  // })
 })
