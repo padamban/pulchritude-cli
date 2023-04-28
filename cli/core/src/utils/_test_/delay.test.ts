6b38b2d372711ac9eb972efe3b79c57d6ccf48c1
Peter Adam <peter.adam.b@gmail.com>
2023-04-29 00:39:01 +0200

    test: try to fix delay test

--- cli/core/src/utils/_test_/delay.test.ts
+++ cli/core/src/utils/_test_/delay.test.ts
@@ -1,6 +1,6 @@
 import { delay } from '../delay'
 
-describe('delay', async () => {
+await describe('delay', async () => {
   await test('delays 100ms', async () => {
     const ts = Date.now()
     await delay(100)
